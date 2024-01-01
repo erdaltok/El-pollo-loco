9512b83d12664304dacf85a9a37b187e5b8b8a4f
Erdal Tok <erdaltok@hotmail.de>
2024-01-01 20:21:06 +0100

    base

---
+++ js/game.js
@@ -0,0 +1,8 @@
+let canvas;
+let character = new Image();
+
+
+function init() {
+    canvas = document.getElementById('canvas');
+    character.src = "/img/2_character_pepe/1_idle/idle/I-1.png";
+}
